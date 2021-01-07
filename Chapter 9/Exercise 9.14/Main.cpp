b715f8d4ad51d1d0186109266bbf3f91bb1aa6c1
Kaan Ozdogan <48806820+Lethalos@users.noreply.github.com>
2021-01-07 10:27:06 +0300

    New operations
    Multiplication, division, and modulus added

--- Chapter 9/Exercise 9.14/Main.cpp
+++ Chapter 9/Exercise 9.14/Main.cpp
@@ -1,29 +1,171 @@
 /*
 	File: Main.cpp
-	Version: 1.0
+	Version: 2.0
 	Writer: Kaan Ozdogan
 	Mail: necdetkaanozdogan@gmail.com
 
 	Description: C++ How to Program 9th Edition by Deitel Chapter 9 Exercise 9.14
-				 This program contains add and subtract operations. Multiply, divide, and modulus will come
+				 This program contains addition substraction, multiplication, divison, and modulus operations
 				 Negative values can be useable
 */
+
 #include"HugeInteger.hpp"
-#include<exception>
 
 int main()
 {
-	HugeInteger* num1 = new HugeInteger();
-	HugeInteger* num2 = new HugeInteger();
+	//positive-positive
+	HugeInteger hugeInt1 = "123";
+	HugeInteger hugeInt2 = "123456";
+	HugeInteger hugeInt3;
 
-	num1->inputHugeInteger();
-	num2->inputHugeInteger();
+	HugeInteger hugeIntZero = "0";
 
-	HugeInteger num3 = *num1 + *num2;
+	std::cout << "== Predicate Operations ==" << std::endl;
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << " hugeIntZero: " << hugeIntZero << std::endl;
+	std::cout << "is hugeIntZero zero: " << ((hugeIntZero.isZero()) ? "True" : "False") << std::endl;
+	std::cout << "is hugeInt1 zero: " << ((hugeInt1.isZero()) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 == hugeInt2: " << ((hugeInt1 == hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 != hugeInt2: " << ((hugeInt1 != hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 > hugeInt2: " << ((hugeInt1 > hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 < hugeInt2: " << ((hugeInt1 < hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 >= hugeInt2: " << ((hugeInt1 >= hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 <= hugeInt2: " << ((hugeInt1 <= hugeInt2) ? "True" : "False") << "\n\n";
 
-	num3.printHugeIntegerWithFormat();
+	std::cout << "== Addition and Substraction ==" << std::endl;
+	hugeInt3 = hugeInt1 + hugeInt2;
+	std::cout << hugeInt1 << " + " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt1 - hugeInt2;
+	std::cout << hugeInt1 << " - " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt2 - hugeInt1;
+	std::cout << hugeInt2 << " - " << hugeInt1 << " = " << hugeInt3 << "\n\n";
 
-	std::cout << "\n\n";
+	//pozitive-negative
+	hugeInt1 = "123456";
+	hugeInt2 = "-987";
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 + hugeInt2;
+	std::cout << hugeInt1 << " + " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt1 - hugeInt2;
+	std::cout << hugeInt1 << " - " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt2 - hugeInt1;
+	std::cout << hugeInt2 << " - " << hugeInt1 << " = " << hugeInt3 << "\n\n";
 
-	num3.printHugeInteger();
+	//negative-positive
+	hugeInt1 = "-123456";
+	hugeInt2 = "987";
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 + hugeInt2;
+	std::cout << hugeInt1 << " + " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt1 - hugeInt2;
+	std::cout << hugeInt1 << " - " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt2 - hugeInt1;
+	std::cout << hugeInt2 << " - " << hugeInt1 << " = " << hugeInt3 << "\n\n";
+
+	//negative-negative
+	hugeInt1 = "-123456";
+	hugeInt2 = "-987";
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 + hugeInt2;
+	std::cout << hugeInt1 << " + " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt1 - hugeInt2;
+	std::cout << hugeInt1 << " - " << hugeInt2 << " = " << hugeInt3 << std::endl;
+	hugeInt3 = hugeInt2 - hugeInt1;
+	std::cout << hugeInt2 << " - " << hugeInt1 << " = " << hugeInt3 << "\n\n";
+
+	//equal
+	hugeInt1 = "123123";
+	hugeInt2 = "123123";
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	std::cout << "hugeInt1 == hugeInt2: " << ((hugeInt1 == hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 != hugeInt2: " << ((hugeInt1 != hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 > hugeInt2: " << ((hugeInt1 > hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 < hugeInt2: " << ((hugeInt1 < hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 >= hugeInt2: " << ((hugeInt1 >= hugeInt2) ? "True" : "False") << std::endl;
+	std::cout << "hugeInt1 <= hugeInt2: " << ((hugeInt1 <= hugeInt2) ? "True" : "False") << "\n\n";
+
+	//exceed 40 digit
+	/*hugeInt1 = "9999999999999999999999999999999999999999";
+	hugeInt2 = "1";
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 + hugeInt2;
+	std::cout << hugeInt1 << " + " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	std::cout << "Please input your huge integer: ";
+	std::cin >> hugeInt1;
+	std::cout << "Huge integer: " << hugeInt1 << std::endl;
+	std::cout << "Size: " << hugeInt1.getSize() << std::endl;
+	*/
+
+	//Multiplication
+	std::cout << "== Multiplication ==\n";
+	
+	//positive-positive
+	hugeInt1 = "12";
+	hugeInt2 = "6";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 * hugeInt2;
+	std::cout << hugeInt1 << " * " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//positive-negative
+	hugeInt1 = "12";
+	hugeInt2 = "-6";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 * hugeInt2;
+	std::cout << hugeInt1 << " * " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//negative-negative
+	hugeInt1 = "-12";
+	hugeInt2 = "-6";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 * hugeInt2;
+	std::cout << hugeInt1 << " * " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//Division
+	std::cout << "\n== Division ==\n";
+
+	//positive-positive
+	hugeInt1 = "64";
+	hugeInt2 = "8";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 / hugeInt2;
+	std::cout << hugeInt1 << " / " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//positive-negative
+	hugeInt1 = "64";
+	hugeInt2 = "-8";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 / hugeInt2;
+	std::cout << hugeInt1 << " / " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//negative-negative
+	hugeInt1 = "-64";
+	hugeInt2 = "-8";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 / hugeInt2;
+	std::cout << hugeInt1 << " / " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//Mod
+	std::cout << "\n== Mod Operation ==\n";
+
+	//positive-positive
+	hugeInt1 = "13";
+	hugeInt2 = "5";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 % hugeInt2;
+	std::cout << hugeInt1 << " % " << hugeInt2 << " = " << hugeInt3 << std::endl;
+
+	//negative-positive
+	hugeInt1 = "-13";
+	hugeInt2 = "5";
+
+	std::cout << "hugeInt1: " << hugeInt1 << " hugeInt2: " << hugeInt2 << std::endl;
+	hugeInt3 = hugeInt1 % hugeInt2;
+	std::cout << hugeInt1 << " % " << hugeInt2 << " = " << hugeInt3 << std::endl;
 }
